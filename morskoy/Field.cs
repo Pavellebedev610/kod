94b192535e5ca50f32c29306bd2c95f5081e7e4a
Pavellebedev610 <104491992+Pavellebedev610@users.noreply.github.com>
2022-10-01 12:20:21 +0300

    qwekjlqjkl

--- morskoy/Field.cs
+++ morskoy/Field.cs
@@ -6,11 +6,40 @@ namespace morskoyboy
 {
     public class Field
     {
-        private byte[,] cells = new byte[10, 10];
+        private byte[,] cells;
         public byte GetCellsValue(byte x, byte y)
         {
             return cells[x, y];
         }
-        public bool SetCellValue 
+        public bool SetCellValue(byte x, byte y, byte value)
+        {
+            if (cells[x, y] != 0)
+            {
+               return false;
+            }
+            cells[x, y] = value;
+            return true;
+        }
+        public Field(int sizex, int sizey)
+        {
+            cells = new byte[sizex, sizey];
+        }
+        public Field(int sizex) : this(sizex, sizex)
+        {
+
+        }
+        public Field() : this(10)
+        {
+
+        }
+        public bool CrashValue(byte x, byte y)
+        {
+            if (cells[x,y] == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        
     }
 }
