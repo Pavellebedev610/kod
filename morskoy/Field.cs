296aa0a341f3c4ba2fab75a52926391b931aab70
Pavellebedev610 <104491992+Pavellebedev610@users.noreply.github.com>
2022-10-01 11:00:34 +0300

    ewqeqw

---
+++ morskoy/Field.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace morskoyboy
+{
+    public class Field
+    {
+        private byte[,] cells = new byte[10, 10];
+        public byte GetCellsValue(byte x, byte y)
+        {
+            return cells[x, y];
+        }
+        public bool SetCellValue 
+    }
+}
